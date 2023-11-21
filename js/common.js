a11f995ef1259e36aa2cfdfce7f502baf69affab
celine <wisohyun0@gmail.com>
2023-11-21 17:45:34 +0900

    경력기술서 업데이트

--- js/common.js
+++ js/common.js
@@ -19,3 +19,52 @@ function classToggle(target) {
     target.classList.remove("active");
   }
 }
+// swiper
+if (document.querySelector(".type-pc") != undefined) {
+  let imgPcSwiper = new Swiper(".img-group.type-pc", {
+    slidesPerView: 1,
+    spaceBetween: 24,
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 24,
+      },
+    },
+    loop: true,
+    autoHeight: true,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
+if (document.querySelector(".type-mo") != undefined) {
+  let imgMoSwiper = new Swiper(".img-group.type-mo", {
+    slidesPerView: 1,
+    spaceBetween: 24,
+    breakpoints: {
+      410: {
+        slidesPerView: 2,
+        spaceBetween: 24,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+    },
+    loop: true,
+    autoHeight: true,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
